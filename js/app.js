dab437b20f0c4be751851972a2af09c5db3e6ba4
Lukasz Plocienniczak <kidmanik69@wp.pl>
2018-08-01 21:58:28 +0200

    upgrade my Page

---
+++ js/app.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+var navi = document.querySelector('#nav1');
+console.log(navi);
+
+navi.addEventListener('click', function (e) {
+    navi = document.querySelector('.sub_nav')
+
+
+})
+
+
+
+});
