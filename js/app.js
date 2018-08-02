bb5a4ec28ec96b0e0929de06233591bad2d658ea
Lukasz Plocienniczak <kidmanik69@wp.pl>
2018-08-02 19:41:14 +0200

    upgrade my Page

--- js/app.js
+++ js/app.js
@@ -1,13 +1,68 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
-var navi = document.querySelector('#nav1');
-console.log(navi);
+    var navi = document.querySelector('#nav1');
+    var subNavi = document.querySelector('.sub_nav');
 
-navi.addEventListener('click', function (e) {
-    navi = document.querySelector('.sub_nav')
+    navi.addEventListener('mouseover', function (e) {
+        subNavi.style.opacity = 1;
+
+        navi.addEventListener('mouseleave', function (e) {
+            subNavi.style.opacity = 0;
+
+        })
+
+    });
+
+    var colsLft = document.querySelector('.column__left');
+    var transparent = document.querySelector('.column__back__transparent');
+
+    colsLft.addEventListener('mouseover',function (e) {
+        transparent.style.display='none';
+
+        colsLft.addEventListener('mouseleave',function (e) {
+            transparent.style.display='inline-block'
+
+        })
+
+    });
+    var colsCen = document.querySelector('.column__center');
+    var transparent2 = document.querySelector('.column__back__transparent2');
+
+    colsCen.addEventListener('mouseover',function (e) {
+        transparent2.style.display='none';
 
 
-})
+        colsCen.addEventListener('mouseleave',function (e) {
+            transparent2.style.display='inline-block';
+
+        })
+
+    });
+
+
+    var prev = document.querySelector('.btn__left');
+    var next = document.querySelector('.btn__right');
+    var picture = document.querySelector('.chair__photo');
+    var sliderTxt = document.querySelector('.slider__text');
+    var indeks = 0;
+
+    next.addEventListener('click', function (e) {
+        indeks++;
+        picture.classList.toggle('chair__photo');
+        picture.classList.toggle('chair__photo2');
+
+
+
+    });
+
+    prev.addEventListener('click', function (e) {
+        indeks--;
+        picture.classList.toggle('chair__photo');
+        picture.classList.toggle('chair__photo2');
+
+    });
+
+
 
 
 
